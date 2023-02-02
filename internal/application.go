16c854a65b666b81c1ad5f8890e21f0da4118d75
ohju96 <ohju96@naver.com>
2023-02-02 23:44:23 +0900

    [WEEK1] : WIP : 오주현 과제 제출(미니앱만들기)

--- internal/application.go
+++ internal/application.go
@@ -8,12 +8,20 @@ func NewApplication(repository Repository) *Application {
 	return &Application{repository: repository}
 }
 
-func (app *Application) Create(request CreateRequest) (CreateResponse, error) {
-	return CreateResponse{"1", "naver"}, nil
+func (app *Application) Create(request CreateRequest) (*CreateResponse, error) {
+	user, err := app.repository.CreateUser(request)
+	if err != nil {
+		return nil, err
+	}
+	return &CreateResponse{user.ID, user.MembershipType}, nil
 }
 
-func (app *Application) Update(request UpdateRequest) (UpdateResponse, error) {
-	return UpdateResponse{}, nil
+func (app *Application) Update(request UpdateRequest) (*UpdateResponse, error) {
+	user, err := app.repository.UpdateUser(request)
+	if err != nil {
+		return nil, err
+	}
+	return &UpdateResponse{user.ID, user.UserName, user.MembershipType}, nil
 }
 
 func (app *Application) Delete(id string) error {
--- internal/repository.go
+++ internal/repository.go
@@ -1,9 +1,83 @@
 package internal
 
+import (
+	"errors"
+	"strconv"
+)
+
 type Repository struct {
 	data map[string]Membership
 }
 
 func NewRepository(data map[string]Membership) *Repository {
 	return &Repository{data: data}
+}
+
+func (r *Repository) CreateUser(request CreateRequest) (*Membership, error) {
+
+	// 이미 등록된 사용자 이름이 존재하면 실패
+	for _, existingUser := range r.data {
+		if existingUser.UserName == request.UserName {
+			return nil, errors.New("이미 존재하는 이름입니다")
+		}
+	}
+
+	// 사용자의 이름을 입력하지 않으면 실패
+	if request.UserName == "" {
+		return nil, errors.New("사용자 이름이 입력되지 않았습니다")
+	}
+
+	// 멤버십 타입을 입력하지 않은 경우 실패
+	if request.MembershipType == "" {
+		return nil, errors.New("멤버십이 입력되지 않았습니다")
+	}
+
+	// naver,toss,payco 이외 타입 입력 시 실패
+	whiteSlice := []string{"naver", "toss", "payco"}
+	if !contains(whiteSlice, request.MembershipType) {
+		return nil, errors.New("허용 안 됩니다")
+	}
+
+	// 멤버십 생성
+	membership := Membership{
+		ID:             strconv.Itoa(len(r.data) + 1),
+		UserName:       request.UserName,
+		MembershipType: request.MembershipType,
+	}
+
+	r.data[membership.ID] = membership
+	return &membership, nil
+}
+
+func (r *Repository) UpdateUser(request UpdateRequest) (*Membership, error) {
+
+	// todo 업데이트 진행 필요
+	membership := r.data[request.ID]
+
+	//// 수정하려는 사용자 이름이 이미 있으면 예외 처리
+	//for _, existingUser := range r.data {
+	//	if existingUser.UserName == request.UserName {
+	//		return nil, errors.New("이미 존재하는 이름입니다")
+	//	}
+	//}
+	//
+	//// 멤버십 아이디 입력하지 않으면 예외
+	//if request.ID == "" {
+	//	return nil, errors.New("멤버십 아이디 입력하지 않음")
+	//}
+
+	newMembership := Membership{membership.ID, request.UserName, request.MembershipType}
+
+	r.data[membership.ID] = newMembership
+	return &newMembership, nil
+}
+
+// 슬라이스 체크용 contains 메서드
+func contains(sliceValues []string, membershipType string) bool {
+	for _, value := range sliceValues {
+		if value == membershipType {
+			return true
+		}
+	}
+	return false
 }
